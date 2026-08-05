e3c85a129db5bda2651deb3115f8666f60e693e6
stann082 <sbennett08@pm.me>
2026-08-05 13:50:39 -0500

    Misc updates

--- deploy.ps1
+++ deploy.ps1
@@ -1,23 +1,9 @@
 remove-item .\pub\ -recurse -erroraction silentlycontinue
-MSBuild .\src\cli\cli.csproj /p:Configuration=Release /p:OutputPath="..\..\build\release\" `
-    /p:Platform="Any CPU" /p:PublishDir="..\..\pub" /t:Publish /v:minimal
+dotnet publish .\src\cli\cli.csproj -c Release -o .\pub
 
-move-item '.\pub\cli.exe' '.\pub\CredManager.exe' -verbose
+move-item '.\pub\cli.exe' '.\pub\credmgr.exe' -verbose
 remove-item $env:APPDATA\utils\cli_1_0_0_0 -recurse -erroraction silentlycontinue
 copy-item '.\pub' "$env:APPDATA\utils\cli_1_0_0_0" -recurse -verbose
-
-$batchScript = @'
-@echo off
-
-set exePath=%APPDATA%\utils\cli_1_0_0_0\CredManager.exe
-if not exist "%exePath%" (
-    echo CredManager.exe not found at %exePath%
-    exit /b 1
-)
-
-"%exePath%" %*
-exit /b %ERRORLEVEL%
-'@
 
 $batchScriptPath = "$env:APPDATA\utils\credmgr.bat"
 if (Test-Path $batchScriptPath) {
